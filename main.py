0e7350225a925d8a909b0ef6592a481a8162a8d1
ANDOI81 <GitHub>
2026-03-06 14:33:25 +0900

    Fix project structure for Heroku

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-# -*- coding: utf-8 -*-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"status": "ok", "message": "FishLog MVP running"}
+from app.main import app  # noqa
